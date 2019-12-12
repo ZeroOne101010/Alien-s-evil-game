a05bebfd70dabd1024905b0222c528984d6710c7
noggato <noggato87@gmail.com>
2019-12-12 23:27:28 +0300

    Update

---
+++ Assets/Scenes/Menu/SelectLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectLevel : MonoBehaviour
+{
+    public GameObject BlackoutScreen;
+    public string Scene;
+    private void Update()
+    {
+        LoadSelectLevel();
+        Debug.Log(BlackoutScreen.GetComponent<Image>().color.a);
+    }
+    public void LoadSelectLevel()
+    {
+        if (BlackoutScreen.GetComponent<Image>().color.a >= 1)
+        {
+            Debug.Log("Соси писюн");
+            SceneManager.LoadScene(Scene, LoadSceneMode.Single);
+        }
+    }
+}
