f86d7d0b65f0977d97fd25271bf11c8dd65cf5a8
noggato <noggato87@gmail.com>
2019-12-21 14:44:12 +0300

    Update

--- Assets/Scenes/Entity/Objects/Guns/Shoot.cs
+++ Assets/Scenes/Entity/Objects/Guns/Shoot.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
+    public GameObject[] Guns = new GameObject[3];
+    Transform FirePoint;
     private void Update()
     {
         
+    }
+    public void Shot()
+    {
+         foreach(GameObject element in Guns)
+        {
+            if (element.active == true)
+            {
+                FirePoint = GetComponentInChildren<Transform>();
+                print(FirePoint.transform.position.x);
+                print(FirePoint.transform.position.y);
+            }
+        }
     }
 }
