87686aa484b91c01803fa9e1f1e50e546febf37b
noggato <noggato87@gmail.com>
2020-01-06 01:15:35 +0300

    Upgrade

---
+++ Assets/Scenes/SelectPlanet/Shop/Script/LevelOfGun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelOfGun : MonoBehaviour
+{
+    public string TypeOfGun;
+    
+    
+
+    
+    void Update()
+    {
+        GetComponent<Text>().text = "Level " + PlayerPrefs.GetInt(TypeOfGun).ToString();
+    }
+}
---
+++ Assets/Scenes/SelectPlanet/Shop/Script/UpgradeGun.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeGun : MonoBehaviour
+{
+    public string TypeGun;
+    public int Cost;
+    public int Multiplier;
+    private void Start()
+    {
+            Cost = (PlayerPrefs.GetInt(TypeGun) * (PlayerPrefs.GetInt(TypeGun) + 1)) * Multiplier / 2;
+    }
+    public void UpgradeOfGun()
+    {
+        if (PlayerPrefs.GetInt("Gold") >= Cost)
+        {
+            PlayerPrefs.SetInt("Gold", PlayerPrefs.GetInt("Gold") - Cost);
+            PlayerPrefs.SetInt(TypeGun, PlayerPrefs.GetInt(TypeGun) + 1);
+            Cost = (PlayerPrefs.GetInt(TypeGun) * (PlayerPrefs.GetInt(TypeGun) + 1)) * Multiplier / 2;
+        }
+    }
+}
