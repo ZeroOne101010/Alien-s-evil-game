b56f25f5410475fad9d01b933a881fc7c1e94d53
Spectre <EmailGax8@Gmail.com>
2020-01-09 00:11:51 +0400

    Доработка Магазина
    Немного доработал магазин, но нужно переделать скрипт WeaponSpecefications и ещё некоторые детали(А может и совсем даже не некоторые)

---
+++ Assets/Scenes/Shop/SectionScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionScript : MonoBehaviour
+{
+    public GameObject section;
+    [HideInInspector]
+    public int sectionIndex;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void ChooseSection()
+    {
+        ShopManager.selectedSection = sectionIndex;
+    }
+}
---
+++ Assets/Scenes/Shop/ShopManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public GameObject[] shopSections;
+    public static int selectedSection;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        SectionLogic();
+    }
+    public void SectionLogic()
+    {
+        for(int i = 0; i < shopSections.Length; i++)
+        {
+            if(shopSections[i].GetComponent<SectionScript>() != null)
+            shopSections[i].GetComponent<SectionScript>().sectionIndex = i;
+
+            //shopSections[i].GetComponent<SectionScript>().section.SetActive(false);
+            if(selectedSection == shopSections[i].GetComponent<SectionScript>().sectionIndex)
+            {
+                shopSections[i].GetComponent<SectionScript>().section.SetActive(true);
+            }
+            else if(selectedSection != shopSections[i].GetComponent<SectionScript>().sectionIndex)
+            {
+                shopSections[i].GetComponent<SectionScript>().section.SetActive(false);
+            }
+        }
+    }
+}
