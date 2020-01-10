c0a50f367b9b41bc7ce80785469ba36678ef48d2
Spectre <EmailGax8@Gmail.com>
2020-01-10 17:32:18 +0400

    Загрузочный экран

--- Assets/Scenes/LoadLevel.cs
+++ Assets/Scenes/LoadLevel.cs
@@ -7,9 +7,12 @@ using UnityEngine.UI;
 public class LoadLevel : MonoBehaviour
 {
     public string scene;
+    public GameObject progressBG;
+
     public void Load_Level()
     {
-            SceneManager.LoadScene(scene, LoadSceneMode.Single);
-        
+        //SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        progressBG.SetActive(true);
+        progressBG.GetComponent<SceneLoadScript>().sceneName = scene;
     }
 }
---
+++ Assets/Scenes/SceneLoadScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoadScript : MonoBehaviour
+{
+    public string sceneName;
+    public Image progressCircle;
+    void Start()
+    {
+        StartCoroutine(AsyncLoad());
+    }
+
+    IEnumerator AsyncLoad()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        while (!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            progressCircle.fillAmount =     progress;
+            yield return null;
+        }
+    }
+
+}
