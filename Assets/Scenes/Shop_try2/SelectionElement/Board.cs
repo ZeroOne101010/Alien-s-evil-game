73bc3f74d9a4c302d5795e3b96d8996593b40c25
Spectre <EmailGax8@Gmail.com>
2020-01-18 23:00:52 +0400

    Заново переделала магазин

---
+++ Assets/Scenes/Shop_try2/SelectionElement/Board.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Board : MonoBehaviour
+{
+    void Start()
+    {
+        BoardStart();
+    }
+
+    void Update()
+    {
+        BoardUpdate();
+    }
+    public virtual void BoardStart()
+    {
+
+    }
+    public virtual void BoardUpdate()
+    {
+
+    }
+}
---
+++ Assets/Scenes/Shop_try2/SelectionElement/BuyBoard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyBoard : Board
+{
+    public GameObject button;
+    public Text price;
+    public GameObject[] initalBoards;
+    public GameObject[] replacementBoards;
+    public override void BoardStart()
+    {
+        base.BoardStart();
+        for (int i = 0; i < replacementBoards.Length; i++)
+            replacementBoards[i].SetActive(false);
+        for (int c = 0; c < initalBoards.Length; c++)
+            initalBoards[c].SetActive(true);
+    }
+    public override void BoardUpdate()
+    {
+        base.BoardUpdate();
+    }
+    public void Buy()
+    {
+        print("Вы приобрели ничто за " + price.text + " золотых стокоратных монет! ПОЗДРАВЛЯЮ");
+        for (int i = 0; i < replacementBoards.Length; i++)
+            replacementBoards[i].SetActive(true);
+        for (int c = 0; c < initalBoards.Length; c++)
+            initalBoards[c].SetActive(false);    
+    }
+}
