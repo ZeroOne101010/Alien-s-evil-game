3b72a084ed8086b8440342b511d904c76bfc7543
KoTeuKaSeeker <emaillit8@gmail.com>
2020-01-19 14:18:14 +0300

    Добавил дробаш

---
+++ Assets/Game/Items/Weapons/Scripts/WeaponShot/ShotgunShot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunShot : WeaponShot
+{
+    public GameObject[] BulletPrefab;
+    public int CountBullets;
+    public float angleShot;
+    public float AngleGap;
+    public Vector2 OffsetShot;
+    public override void shot(Vector3 offsetShot)
+    {
+        GameObject[] Bullet = new GameObject[CountBullets];
+        for (int i = 0; i < CountBullets; i++)
+        {
+            int bulletPrifabId = Random.Range(0, BulletPrefab.Length);
+            float isRight = getKeepedEntity().GetComponent<EntityMove>().isRight ? 1 : -1;
+            Vector2 spawnPos = (Vector2)(transform.position) + OffsetShot * isRight;
+            Bullet[i] = Instantiate(BulletPrefab[bulletPrifabId], spawnPos, Quaternion.identity);
+            BulletController bulletController = Bullet[i].GetComponent<BulletController>();
+            float randomK = Random.Range(0.000f, 1.000f);
+            float angle = (AngleGap) * randomK - (AngleGap / 2);
+            float radAngle = (((angleShot + angle) / 360) * 2 * Mathf.PI);
+            Vector2 direction = new Vector2(Mathf.Cos(radAngle), Mathf.Sin(radAngle)) * isRight;
+            bulletController.initBullet(getTeamId(), direction, gameObject, this);
+        }
+    }
+}
