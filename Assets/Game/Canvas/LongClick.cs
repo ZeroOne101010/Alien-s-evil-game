afb5e0a3a0f2921ea568681250b6082f40c03a72
noggato <noggato87@gmail.com>
2020-01-29 23:28:17 +0300

    LongClicl and Canvas

---
+++ Assets/Game/Canvas/LongClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LongClick : MonoBehaviour
+{
+    public bool Down = false;
+    public GameObject Person;
+    public void Update()
+    {
+        if (Down == true)
+        {
+            Person.GetComponent<EntityAttack>().attack(null);
+        }
+    }
+    public void ChangeDownToTrue()
+    {
+        Down = true;
+    }
+    public void ChangeDownToFalse()
+    {
+        Down = false;
+    }
+}
---
+++ Assets/Game/Canvas/ScriptOfPause.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptOfPause : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
