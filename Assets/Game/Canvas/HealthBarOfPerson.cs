4b1cc4c0839319b7f3bf2e3aa49d34e7f5875fc6
noggato <noggato87@gmail.com>
2020-01-30 00:39:21 +0300

    Update

---
+++ Assets/Game/Canvas/HealthBarOfPerson.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarOfPerson : MonoBehaviour
+{
+    public Slider slide;
+    public GameObject Person;
+    private void Start()
+    {
+        slide.maxValue = Person.GetComponent<UserEntityController>().health;
+    }
+    private void Update()
+    {
+        slide.value = Person.GetComponent<UserEntityController>().health;
+    }
+}
